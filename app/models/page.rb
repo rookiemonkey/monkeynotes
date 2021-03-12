f21427e8a7e8c2bfa7ec9836585db7bf5aa20db8
rookiemonkey <kevinroirigorbasina@pm.me>
2021-03-12 11:09:41 +0800

    :heavy_check_mark: add page specs

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,3 +1,6 @@
 class Page < ApplicationRecord
   has_rich_text :content
+
+  validates :subject, length: { within: 2..20 }
+  validates :content, presence: true
 end
--- test/models/page_test.rb
+++ test/models/page_test.rb
@@ -1,7 +1,28 @@
 require "test_helper"
 
 class PageTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @error_message = 'PAGE Model should'
+    @page = Page.create(subject: 'Sample', content: '<p>Sample</p>')
+  end
+
+  test 'should reject empty subject' do
+    error_message = "#{@error_message} reject empty subject"
+    @page.subject = nil
+    assert_not @page.save, error_message
+  end
+
+  test 'should reject subject w/ more than 20 chars' do
+    error_message = "#{@error_message} reject subject w/ more than 20 chars"
+    @page.subject = ('a'*21)
+    assert_not @page.save, error_message
+  end
+
+  test 'should reject empty content' do
+    error_message = "#{@error_message} reject empty content"
+    @page.content = nil
+    assert_not @page.save, error_message
+  end
+
 end
