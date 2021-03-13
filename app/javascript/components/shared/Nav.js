0eafa3d80f76fbfbad3aa1f76f0f847e10c72611
rookiemonkey <kevinroirigorbasina@pm.me>
2021-03-13 19:01:06 +0800

    :collision: initial styles

---
+++ app/javascript/components/shared/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <nav className="uk-navbar-container uk-margin uk-navbar-transparent" uk-navbar="true">
+      <div className="uk-navbar-center">
+        <a className="uk-navbar-item uk-logo" href="#">
+          Monkeynotes
+        </a>
+      </div>
+    </nav>
+  )
+}
+
+export default Nav
