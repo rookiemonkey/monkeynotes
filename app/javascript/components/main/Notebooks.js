0d22f5520a92e228b91e2378b39c40d02aedee54
rookiemonkey <kevinroirigorbasina@pm.me>
2021-03-14 12:30:18 +0800

    :collision: initial http request works

--- app/javascript/components/main/Notebooks.js
+++ app/javascript/components/main/Notebooks.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Nav from '../shared/Nav'
 
 const Notebooks = () => {
+  const [notebooks, setNotebooks] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const raw = await fetch('/notebook/all')
+      const data = raw.json()
+      setNotebooks(data)
+    })()
+  }, [])
+
   return (
     <React.Fragment>
       <Nav />
-      <h1>Notebooks w!</h1>
+
     </React.Fragment>
   )
 }
